06237881ad2f221d3e03122bbd4bc5b7c5a38c31
togayyazar <uyazar@etu.edu.tr>
2018-02-16 22:30:42 +0300

    Getting ready for deployment to heroku

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,4 +1,17 @@
 package com.mycompany.app;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+
+
 import java.util.ArrayList;
 
 /**
@@ -7,11 +20,6 @@ import java.util.ArrayList;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-
 	public static boolean bothContain(Integer[] arry1,ArrayList<Integer> arry2,int value,boolean addToArray) {
 		/*This method checks whether given arrays contain the value.If both of them contains the value,method returns true.
 		 *addToArray parameter decides that whether method adds the value to arrays which does not contain the value.
@@ -77,4 +85,71 @@ public class App
 			else
 				System.out.println("Since Integer[] is full,no space to add value, adding operation could not be performed on Integer[] array.");
 	}
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+	System.out.println("0");	
+        get("/", (req, res) -> "Hello, World");
+	System.out.println("1");
+        post("/compute", (req, res) -> {
+          //System.out.println(req.queryParams("input1"));
+          //System.out.println(req.queryParams("input2"));
+			System.out.println("3");
+          String input1 = req.queryParams("input1");
+          java.util.Scanner sc1 = new java.util.Scanner(input1);
+          sc1.useDelimiter("[;\r\n]+");
+          java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
+          while (sc1.hasNext())
+          {
+            int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+            inputList.add(value);
+          }
+          System.out.println(inputList);
+
+          String input2 = req.queryParams("input2");
+          java.util.Scanner sc2 = new java.util.Scanner(input2);
+          sc2.useDelimiter("[;\r\n]+");
+          Integer[] inputList2 = new Integer[10];
+	  int t=0;
+          while (sc2.hasNext())
+          {
+            int value = Integer.parseInt(sc2.next().replaceAll("\\s",""));
+            inputList2[t]=value;
+	    t++;
+          }
+          System.out.println(inputList2);
+
+
+          String input3 = req.queryParams("input3").replaceAll("\\s","");
+          int input3AsInt = Integer.parseInt(input3);
+
+          String input4 = req.queryParams("input4").replaceAll("\\s","");
+          boolean input4AsBoolean = Boolean.parseBoolean(input4);
+
+          boolean result = App.bothContain(inputList2, inputList,input3AsInt,input4AsBoolean);
+
+         Map map = new HashMap();
+          map.put("result", result);
+          return new ModelAndView(map, "compute.mustache");
+        }, new MustacheTemplateEngine());
+
+
+        get("/compute",
+            (rq, rs) -> {
+              Map map = new HashMap();
+              map.put("result", "not computed yet!");
+              return new ModelAndView(map, "compute.mustache");
+            },
+            new MustacheTemplateEngine());
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+ 
 }
+
